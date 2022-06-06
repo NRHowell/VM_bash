3f89ff9ebc4d64936897f41013345f3d01b67709
Nick Howell <77030938+NRHowell@users.noreply.github.com>
2022-06-06 14:14:50 +1000

    Update ubuntu_VM_setup_nrh.sh

--- ubuntu_VM_setup_nrh.sh
+++ ubuntu_VM_setup_nrh.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#Script for initial setup of Ubuntu VM
+#Script for initial setup of Ubuntu VM's with Anaconda, ImageJ and Github
 
 
 sudo apt update
@@ -14,6 +14,9 @@ sudo apt-get install atop wget neofetch libgl1-mesa-glx libegl1-mesa libxrandr2 
 wget -P ~/Downloads/  https://downloads.imagej.net/fiji/latest/fiji-linux64.zip
 unzip ~/Downloads/fiji-linux64.zip -d ~/
 
+#install github desktop
+sudo wget https://github.com/shiftkey/desktop/releases/download/release-2.9.3-linux3/GitHubDesktop-linux-2.9.3-linux3.deb
+sudo gdebi GitHubDesktop-linux-2.9.3-linux3.deb
 
 #install anaconda
 wget https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh
