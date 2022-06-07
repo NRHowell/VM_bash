7b15e01ffb02ae35d5980efec1105e55033c5ecf
Nick Howell <77030938+NRHowell@users.noreply.github.com>
2022-06-07 13:41:24 +1000

    Update ubuntu_VM_setup_nrh.sh

--- ubuntu_VM_setup_nrh.sh
+++ ubuntu_VM_setup_nrh.sh
@@ -22,5 +22,7 @@ sudo gdebi GitHubDesktop-linux-2.9.3-linux3.deb
 wget https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh
 bash Anaconda3-2022.05-Linux-x86_64.sh
 
+#close current terminal to complete anaconda install
+gnome-terminal
 exit
 
