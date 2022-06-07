b6686760e38b3a932f830eeb422f73145fab4724
Nick Howell <77030938+NRHowell@users.noreply.github.com>
2022-06-07 14:54:27 +1000

    Update ubuntu_VM_setup_nrh.sh

--- ubuntu_VM_setup_nrh.sh
+++ ubuntu_VM_setup_nrh.sh
@@ -12,7 +12,7 @@ sudo apt update
 sudo apt-get upgrade
 
 #install required packages
-sudo apt-get install atop wget neofetch libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 unzip
+sudo apt-get install atop wget neofetch libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 unzip gdebi
 
 #install image j
 wget -P ~/Downloads/  https://downloads.imagej.net/fiji/latest/fiji-linux64.zip
