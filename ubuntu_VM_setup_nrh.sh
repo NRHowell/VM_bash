6b29a5e4b26217e8be916ccf0d52d386be800a0d
Nick Howell <77030938+NRHowell@users.noreply.github.com>
2022-06-24 10:48:23 +1000

    Update ubuntu_VM_setup_nrh.sh

--- ubuntu_VM_setup_nrh.sh
+++ ubuntu_VM_setup_nrh.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-#Script for initial setup of Ubuntu VM's with Anaconda, ImageJ and Github
-#Author: nrh
+#Script for initial setup of Ubuntu VM's with Anaconda, ImageJ, Atom, RStudio and Github
+#Author: Nick Howell
 
 
 #change the XFCE theme
@@ -12,7 +12,7 @@ sudo apt update
 sudo apt-get upgrade
 
 #install required packages
-sudo apt-get install atop wget neofetch libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 unzip gdebi-core vim-gt3
+sudo apt-get install atop wget neofetch libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 unzip gdebi-core vim-gt3 r-base
 
 #install atom
 sudo snap install atom --classic
@@ -25,6 +25,9 @@ unzip ~/Downloads/fiji-linux64.zip -d ~/
 wget https://github.com/shiftkey/desktop/releases/download/release-2.9.3-linux3/GitHubDesktop-linux-2.9.3-linux3.deb
 sudo gdebi GitHubDesktop-linux-2.9.3-linux3.deb
 
+#install RStudio
+wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-2022.02.3-492-amd64.deb
+sudo gdebi rstudio-server-2022.02.3-492-amd64.deb
 
 #install anaconda
 wget https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh
